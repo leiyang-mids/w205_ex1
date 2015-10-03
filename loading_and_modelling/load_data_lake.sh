950ff51004370413e4f6bb7a949d101fe1146595
leiyang-mids <ynglei@gmail.com>
2015-10-03 21:22:41 +0000

    debug of the script

--- loading_and_modelling/load_data_lake.sh
+++ loading_and_modelling/load_data_lake.sh
@@ -1,25 +1,31 @@
-# create local folder to download and store the data
+#! /bin/bash
+
+echo "# create local folder to download and store the data\n"
 mkdir /data/lei_ex1
 cd /data/lei_ex1
 
-# download and unzip the data
+echo "# download and unzip the data\n"
 wget https://data.medicare.gov/views/bg9k-emty/files/Nqcy71p9Ss2RSBWDmP77H1DQXcyacr2khotGbDHHW_s?content_type=application%2Fzip%3B%20charset%3Dbinary&filename=Hospital_Revised_Flatfiles.zip
+
+wait
+
 unzip Nqc*
 
-# get rid of header and rename files
+echo "# get rid of header and rename files"
 tail -n +2 "Hospital General Information.csv" > "hospitals.csv"
 tail -n +2 "Timely and Effective Care - Hospital.csv" > "effective_care.csv"
 tail -n +2 "Readmissions and Deaths - Hospital.csv" > "readmissions.csv"
 tail -n +2 "Measure Dates.csv" > "measure_dates.csv"
 tail -n +2 "hvbp_hcahps_05_28_2015.csv" > "surveys_responses.csv"
 
-# create folder in HDFS
-hdfs dfs -mkdir /user/lei_ex1
-hdfs dfs -mkdir /user/lei_ex1/hospital_compare
+echo "# create /user/w205/hospital_compare folder in HDFS"
+hdfs dfs -mkdir /user/w205/hospital_compare
 
-# load the raw data files into HDFS under “/user/lei_ex1/hospital_compare”
-hdfs dfs -put hospitals.csv /user/lei_ex1/hospital_compare/hospitals.csv
-hdfs dfs -put effective_care.csv /user/lei_ex1/hospital_compare/effective_care.csv
-hdfs dfs -put readmissions.csv /user/lei_ex1/hospital_compare/readmissions.csv
-hdfs dfs -put measure_dates.csv /user/lei_ex1/hospital_compare/measure_dates.csv
-hdfs dfs -put surveys_responses.csv /user/lei_ex1/hospital_compare/surveys_responses.csv
+echo "# load the raw data files into HDFS under /user/w205/hospital_compare"
+hdfs dfs -put hospitals.csv /user/w205/hospital_compare/hospitals.csv
+hdfs dfs -put effective_care.csv /user/w205/hospital_compare/effective_care.csv
+hdfs dfs -put readmissions.csv /user/w205/hospital_compare/readmissions.csv
+hdfs dfs -put measure_dates.csv /user/w205/hospital_compare/measure_dates.csv
+hdfs dfs -put surveys_responses.csv /user/w205/hospital_compare/surveys_responses.csv
+
+echo "# data loading successfully completed!"
