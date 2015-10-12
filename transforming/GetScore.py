794df4dd4bb6061c7b732e62ff46b34e942f72cf
Lei Yang <lei.yng@gmail.com>
2015-10-12 14:24:55 -0400

    Create GetScore.py
    transfer/convert "x out of y" entry into numeric value

---
+++ transforming/GetScore.py
@@ -0,0 +1,8 @@
+def GetScore(OutOfString):
+    try:
+        elem = [x for x in OutOfString.lower().split(' ') if x is not '']        
+        if len(elem) != 4 or elem[1]+elem[2] != "outof":  #'out' not in elem or 'of' not in elem:
+            return None
+        return float(elem[0])/float(elem[-1])
+    except:
+        return None 
