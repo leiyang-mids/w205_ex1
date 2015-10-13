8ac29c3f39e239c3bb68e8c94bbcd64d5d2ab528
Lei Yang <lei.yng@gmail.com>
2015-10-13 11:57:34 -0400

    add query to evaluate procedure variability

--- investigations/hospital_variability/hospital_variability.sql
+++ investigations/hospital_variability/hospital_variability.sql
@@ -1,1 +1,9 @@
+-- evaluate bsed on effective care
 select
+  variance(e.score) as var_score,
+  m.id, m.name
+from m_effective e left join m_measure m
+on e.m_id = m.id
+group by m.id, m.name
+order by var_score desc
+limit 10;
