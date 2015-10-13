9b2b598eaff9f850e5b9b49590ccacf851f5a4c7
root <root@ip-172-31-14-43.ec2.internal>
2015-10-13 04:34:02 +0000

    best hospital and state query

--- investigations/best_hospitals/best_hospitals.sql
+++ investigations/best_hospitals/best_hospitals.sql
@@ -1,2 +1,20 @@
 -- join readmission, effective, hospital table, calculate average score
-select 
+select
+   avg(e.score) as avg_score,
+   h.name, h.city, h.state
+from m_effective e left join m_hospital h
+on e.h_id = h.id
+group by h.name, h.city, h.state
+order by avg_score desc
+limit 20; 
+
+select
+  avg(r.score) as avg_score,
+  h.name, h.city, h.state
+from m_readmission r left join m_hospital h
+on r.h_id = h.id
+group by h.state, h.city, h.name
+having avg_score is not null
+order by avg_score
+limit 20;
+
--- investigations/best_states/best_states.sql
+++ investigations/best_states/best_states.sql
@@ -1,1 +1,8 @@
 select
+   h.state,
+   avg(e.score) as avg_score
+from m_effective e left join m_hospital h
+on e.h_id = h.id
+group by h.state
+order by avg_score desc
+limit 20; 
