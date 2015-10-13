6fb911cebe46d79d14837c6872aebebd76a7d67f
Lei Yang <lei.yng@gmail.com>
2015-10-13 11:11:28 -0400

    add sql for merging readmission and effective

--- investigations/best_hospitals/best_hospitals.sql
+++ investigations/best_hospitals/best_hospitals.sql
@@ -1,4 +1,4 @@
--- join readmission, effective, hospital table, calculate average score
+-- evaluate based on effective care
 select
    avg(e.score) as avg_score,
    h.name, h.city, h.state
@@ -6,8 +6,9 @@ from m_effective e left join m_hospital h
 on e.h_id = h.id
 group by h.name, h.city, h.state
 order by avg_score desc
-limit 20; 
+limit 10; 
 
+-- evaluate based on readmission
 select
   avg(r.score) as avg_score,
   h.name, h.city, h.state
@@ -16,5 +17,23 @@ on r.h_id = h.id
 group by h.state, h.city, h.name
 having avg_score is not null
 order by avg_score
-limit 20;
+limit 10;
 
+-- evaluate based on combination of readmission and effective care
+from (
+select 
+  h.state, h.city, h.name,
+  case 
+	when r.score is null and e.score is null then null
+	when r.score is null and e.score is not null then 100*e.score/max(e.score)
+	when r.score is not null and e.score is null then 100-r.score
+	else ((100-r.score)+100*e.score/max(e.score))/2
+  end as overall_score
+  from 
+	m_hospital h right join m_effective e on h.id = e.h_id
+	left join m_readmission r on e.h_id=r.h_id and e.m_id=r.m_id
+) m
+select m.state, m.city, m.name, avg(m.overall_score) as avg_score
+group by m.state, m.city, m.name
+order by avg_score desc
+limit 10;
