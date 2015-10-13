f900520aacd411c3e0e80942afb1e782bb27191e
root <root@ip-172-31-14-43.ec2.internal>
2015-10-13 04:37:42 +0000

    best state on readmission

--- investigations/best_states/best_states.sql
+++ investigations/best_states/best_states.sql
@@ -6,3 +6,13 @@ on e.h_id = h.id
 group by h.state
 order by avg_score desc
 limit 20; 
+
+select
+   h.state,
+   avg(r.score) as avg_score
+from m_readmission r left join m_hospital h
+on e.h_id = h.id
+group by h.state
+having avg_sore is not null
+order by avg_score desc
+limit 20;
