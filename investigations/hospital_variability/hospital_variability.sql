11615d8acc0aa52f98b14b6a8a2cfd1cf4a6b707
LEI YANG <lei.yng@gmail.com>
2015-10-18 16:55:54 -0400

    redo variance

--- investigations/hospital_variability/hospital_variability.sql
+++ investigations/hospital_variability/hospital_variability.sql
@@ -1,9 +1,37 @@
--- evaluate bsed on effective care
+-- Similar with previous evaluation, it doesn't make sense to compare
+-- the absolute variance value between the measure (procedure),
+-- therefore we still normalize each measure before ranking the variance.
+-- And here we focus on effective score and don't consider readmission rate
+
+-- SQL statement:
+from(
+  from(
+    -- step 1: get max effective score for each measure item for normalization
+    from(
+      select
+        e.m_id,
+        max(e.score) as max_score
+      from m_effective e
+      group by e.m_id
+    ) norm
+    -- step 2a: join with effective table by measure_id and
+    right join m_effective e on e.m_id = norm.m_id
+    select
+      -- step 2b: get normalized score
+      e.score/norm.max_score as norm_score,
+      e.m_id
+  ) norm_score
+  select
+    -- step 4: calculate the average join score for each hospital
+    cast(variance(norm_score) AS DECIMAL(10,4)) as final_score,
+    m_id
+  group by m_id
+) final
+left join m_measure m on final.m_id = m.id
+-- step 5: rank the final score, there are a bunch ties, so we show the top 100
 select
-  m.id, m.name,
-  variance(e.score) as var_score
-from m_effective e left join m_measure m
-on e.m_id = m.id
-group by m.id, m.name
-order by var_score desc
+  rank() OVER (
+      ORDER by final_score desc
+  ) as rank,
+  final_score as score, m_id as measure_id, m.name as name
 limit 10;
